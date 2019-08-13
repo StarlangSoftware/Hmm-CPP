2a5c96c5047791e7bdf503b257516beccab4d1b8
Olcay Taner YILDIZ <olcaytaner@isikun.edu.tr>
2019-08-13 18:56:15 +0300

    Added documentation.

--- Hmm2.h
+++ Hmm2.h
@@ -27,6 +27,14 @@ public:
 template<class State, class Symbol> Hmm2<State, Symbol>::Hmm2() {
 }
 
+/**
+ * calculatePi calculates the prior probability matrix (initial probabilities for each state combinations)
+ * from a set of observations. For each observation, the function extracts the first and second states in
+ * that observation.  Normalizing the counts of the pair of states returns us the prior probabilities for each
+ * pair of states.
+ *
+ * @param observations A set of observations used to calculate the prior probabilities.
+ */
 template<class State, class Symbol> void Hmm2<State, Symbol>::calculatePi(int observationCount, vector<State> *observations) {
     pi = Matrix(this->stateCount, this->stateCount);
     for (int i = 0; i < observationCount; i++) {
@@ -38,6 +46,13 @@ template<class State, class Symbol> void Hmm2<State, Symbol>::calculatePi(int ob
     pi.columnWiseNormalize();
 }
 
+/**
+ * calculateTransitionProbabilities calculates the transition probabilities matrix from each state to another state.
+ * For each observation and for each transition in each observation, the function gets the states. Normalizing the
+ * counts of the three of states returns us the transition probabilities.
+ *
+ * @param observations A set of observations used to calculate the transition probabilities.
+ */
 template<class State, class Symbol> void Hmm2<State, Symbol>::calculateTransitionProbabilities(int observationCount, vector<State> *observations) {
     this->transitionProbabilities = Matrix(this->stateCount * this->stateCount, this->stateCount);
     for (int i = 0; i < observationCount; i++) {
@@ -52,6 +67,12 @@ template<class State, class Symbol> void Hmm2<State, Symbol>::calculateTransitio
     this->transitionProbabilities.columnWiseNormalize();
 }
 
+/**
+ * logOfColumn calculates the logarithm of each value in a specific column in the transition probability matrix.
+ *
+ * @param column Column index of the transition probability matrix.
+ * @return A vector consisting of the logarithm of each value in the column in the transition probability matrix.
+ */
 template<class State, class Symbol> Vector Hmm2<State, Symbol>::logOfColumn(int column) {
     Vector result = Vector(0, 0.0);
     int i;
@@ -61,6 +82,12 @@ template<class State, class Symbol> Vector Hmm2<State, Symbol>::logOfColumn(int 
     return result;
 }
 
+/**
+ * viterbi calculates the most probable state sequence for a set of observed symbols.
+ *
+ * @param s A set of observed symbols.
+ * @return The most probable state sequence as an {@link ArrayList}.
+ */
 template<class State, class Symbol> vector<State> Hmm2<State, Symbol>::viterbi(vector<Symbol> s) {
     int i, j, t, maxIndex;
     int sequenceLength = s.size();
@@ -106,6 +133,15 @@ template<class State, class Symbol> vector<State> Hmm2<State, Symbol>::viterbi(v
     return result;
 }
 
+/**
+ * A constructor of {@link Hmm2} class which takes a {@link Set} of states, an array of observations (which also
+ * consists of an array of states) and an array of instances (which also consists of an array of emitted symbols).
+ * The constructor calls its super method to calculate the emission probabilities for those states.
+ *
+ * @param states A {@link Set} of states, consisting of all possible states for this problem.
+ * @param observations An array of instances, where each instance consists of an array of states.
+ * @param emittedSymbols An array of instances, where each instance consists of an array of symbols.
+ */
 template<class State, class Symbol> Hmm2<State, Symbol>::Hmm2(unordered_set<State> states, int observationCount, vector<State> *observations, vector<Symbol> *emittedSymbols) {
     int i = 0;
     this->stateCount = states.size();
