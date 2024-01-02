06d01d5cf9fd68783fd18d2e5da6b72a21988113
Olcay Taner YILDIZ <olcay.yildiz@ozyegin.edu.tr>
2024-01-02 09:34:31 +0300

    Updated CMakeLists.txt files and conanfile.py file for Conan 2.0 compatibility.

--- conanfile.py
+++ conanfile.py
@@ -1,18 +1,20 @@
-from conans import ConanFile, CMake, tools
+from os.path import join
+
+from conan import ConanFile
+from conan.tools.files import copy
 
 
 class HmmConan(ConanFile):
-    name = "Hmm"
+    name = "hmm"
     version = "1.0.0"
-    requires = ["Math/1.0.0", "DataStructure/1.0.0"]
+    requires = ["math/1.0.0", "data_structure/1.0.0"]
     license = "GPL License"
     author = "Olcay Taner Yildiz olcay.yildiz@ozyegin.edu.tr"
     url = "https://github.com/StarlangSoftware/Math-CPP"
     description = "Hmm library"
     topics = ("")
-    generators = "cmake"
     exports_sources = "src/*"
     no_copy_source = True
 
     def package(self):
-        self.copy("*.h", dst="include", keep_path=False)
+        copy("*.h", keep_path=False, src=join(self.source_folder), dst=join(self.package_folder, "include"), pattern="*.h")
